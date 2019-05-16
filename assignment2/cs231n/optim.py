be9a8b8d5fd509e410b305e6172652a56b83c55f
Yinhao Li <liyinhao0413@qq.com>
2019-05-16 19:27:37 +0800

    finish assignment1

--- assignment2/cs231n/optim.py
+++ assignment2/cs231n/optim.py
@@ -100,6 +100,7 @@ def rmsprop(w, dw, config=None):
     # in the next_w variable. Don't forget to update cache value stored in    #
     # config['cache'].                                                        #
     ###########################################################################
+    # 原理:https://zhuanlan.zhihu.com/p/34230849
     config['cache'] = config['cache'] * config['decay_rate'] + (1 - config['decay_rate']) * dw ** 2
     next_w = w - config['learning_rate'] * dw / (np.sqrt(config['cache']) + config['epsilon'])
     ###########################################################################
@@ -141,6 +142,7 @@ def adam(w, dw, config=None):
     # NOTE: In order to match the reference output, please modify t _before_  #
     # using it in any calculations.                                           #
     ###########################################################################
+    #将rms和momentem结合
     config['m'] = config['beta1'] * config['m'] + (1 - config['beta1']) * dw
     config['v'] = config['beta2'] * config['v'] + (1 - config['beta2']) * (dw ** 2)
 
