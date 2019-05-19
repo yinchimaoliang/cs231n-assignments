bd7806d1d52d46003ef918243308bef82e995191
Yinhao Li <liyinhao0413@qq.com>
2019-05-19 20:02:18 +0800

    finish lstm

--- assignment3/cs231n/rnn_layers.py
+++ assignment3/cs231n/rnn_layers.py
@@ -301,107 +301,191 @@ def lstm_step_forward(x, prev_h, prev_c, Wx, Wh, b):
     # TODO: Implement the forward pass for a single timestep of an LSTM.        #
     # You may want to use the numerically stable sigmoid implementation above.  #
     #############################################################################
-    pass
+    N, H = prev_h.shape
+    A = x.dot(Wx) + prev_h.dot(Wh) + b
+    ai = A[:, 0:H]
+    af = A[:, H:2 * H]
+    ao = A[:, 2 * H:3 * H]
+    ag = A[:, 3 * H:4 * H]
+
+    i = sigmoid(ai)
+    f = sigmoid(af)
+    o = sigmoid(ao)
+    g = np.tanh(ag)
+
+    next_c = np.multiply(f, prev_c) + np.multiply(i, g)
+    next_h = np.multiply(o, np.tanh(next_c))
+
+    cache = (x, prev_h, prev_c, i, f, o, g, Wx, Wh, next_c, A)
+    return next_h, next_c, cache
     ##############################################################################
     #                               END OF YOUR CODE                             #
     ##############################################################################
-
-    return next_h, next_c, cache
 
 
 def lstm_step_backward(dnext_h, dnext_c, cache):
     """
-    Backward pass for a single timestep of an LSTM.
+    输入:
+    - dnext_h: 下一层传来关于h的梯度, 维度 (N, H)
+    - dnext_c: 下一层传来关于c的梯度, 维度 (N, H)
+    - cache: 前向传播存的变量
 
-    Inputs:
-    - dnext_h: Gradients of next hidden state, of shape (N, H)
-    - dnext_c: Gradients of next cell state, of shape (N, H)
-    - cache: Values from the forward pass
-
-    Returns a tuple of:
-    - dx: Gradient of input data, of shape (N, D)
-    - dprev_h: Gradient of previous hidden state, of shape (N, H)
-    - dprev_c: Gradient of previous cell state, of shape (N, H)
-    - dWx: Gradient of input-to-hidden weights, of shape (D, 4H)
-    - dWh: Gradient of hidden-to-hidden weights, of shape (H, 4H)
-    - db: Gradient of biases, of shape (4H,)
+    输出
+    - dx: 输入x的梯度, 维度(N, D)
+    - dprev_h: 上一层隐藏状态的梯度, 维度(N, H)
+    - dprev_c: 上一层单元状态的梯度, 维度(N, H)
+    - dWx: 权值矩阵Wxh的梯度, 维度(D, 4H)
+    - dWh: 权值矩阵Whh的梯度, 维度(H, 4H)
+    - db: 偏差值b的梯度，维度（4H,）
     """
-    dx, dprev_h, dprev_c, dWx, dWh, db = None, None, None, None, None, None
-    #############################################################################
-    # TODO: Implement the backward pass for a single timestep of an LSTM.       #
-    #                                                                           #
-    # HINT: For sigmoid and tanh you can compute local derivatives in terms of  #
-    # the output value from the nonlinearity.                                   #
-    #############################################################################
-    pass
-    ##############################################################################
-    #                               END OF YOUR CODE                             #
-    ##############################################################################
+    #提取cache中的变量
+    N,H=dnext_h.shape
+    f=cache[4]
+    o=cache[5]
+    i=cache[3]
+    g=cache[6]
+    nc=cache[9]
+    prev_c=cache[2]
+    prev_x=cache[0]
+    prev_h=cache[1]
+    A=cache[10]
+    ai=A[:,0:H]
+    af=A[:,H:2*H]
+    ao=A[:,2*H:3*H]
+    ag=A[:,3*H:4*H]
+    Wx=cache[7]
+    Wh=cache[8]
+    #计算到c_t-1的梯度
+    dc_c=np.multiply(dnext_c,f)
+    dc_h_temp=np.multiply(dnext_h,o)
+    temp = np.ones_like(nc)-np.square(np.tanh(nc))
+    temp2=np.multiply(temp,f)
+    dprev_c=np.multiply(temp2,dc_h_temp)+dc_c
+
+    #计算(dE/dh)(dh/dc)
+    dc_from_h=np.multiply(dc_h_temp,temp)
+
+    dtotal_c=dc_from_h+dnext_c
+
+    #计算到o,f,i,g的梯度
+    tempo=np.multiply(np.tanh(nc),dnext_h)
+    tempf=np.multiply(dtotal_c,prev_c)
+    tempi=np.multiply(dtotal_c,g)
+    tempg=np.multiply(dtotal_c,i)
+
+    #计算到ao,ai,af,ag的梯度
+    tempao=np.multiply(tempo,np.multiply(o,np.ones_like(o)-o))
+    tempai=np.multiply(tempi,np.multiply(i,np.ones_like(o)-i))
+    tempaf=np.multiply(tempf,np.multiply(f,np.ones_like(o)-f))
+    dtanhg = np.ones_like(ag)-np.square(np.tanh(ag))
+    tempag=np.multiply(tempg,dtanhg)
+
+    #计算各参数的梯度
+    TEMP=np.concatenate((tempai,tempaf,tempao,tempag),axis=1)
+    dx=TEMP.dot(Wx.T)
+    dprev_h=TEMP.dot(Wh.T)
+    xt=prev_x.T
+    dWx=xt.dot(TEMP)
+    ht=prev_h.T
+    dWh=ht.dot(TEMP)
+    db=np.sum(TEMP,axis=0).T
 
     return dx, dprev_h, dprev_c, dWx, dWh, db
 
 
+
 def lstm_forward(x, h0, Wx, Wh, b):
     """
-    Forward pass for an LSTM over an entire sequence of data. We assume an input
-    sequence composed of T vectors, each of dimension D. The LSTM uses a hidden
-    size of H, and we work over a minibatch containing N sequences. After running
-    the LSTM forward, we return the hidden states for all timesteps.
 
-    Note that the initial cell state is passed as input, but the initial cell
-    state is set to zero. Also note that the cell state is not returned; it is
-    an internal variable to the LSTM and is not accessed from outside.
+    输入:
+    - x: 整个序列的输入数据, 维度 (N, T, D).
+    - h0: 初始隐藏层, 维度 (N, H)
+    - Wx: 权值矩阵Wxh, 维度 (D, H)
+    - Wh: 权值矩阵Whh, 维度 (H, H)
+    - b: 偏差值，维度 (H,)
 
-    Inputs:
-    - x: Input data of shape (N, T, D)
-    - h0: Initial hidden state of shape (N, H)
-    - Wx: Weights for input-to-hidden connections, of shape (D, 4H)
-    - Wh: Weights for hidden-to-hidden connections, of shape (H, 4H)
-    - b: Biases of shape (4H,)
+    输出:
+    - h: 整个序列的隐藏状态, 维度 (N, T, H).
+    - cache: 反向传播时需要的变量
 
-    Returns a tuple of:
-    - h: Hidden states for all timesteps of all sequences, of shape (N, T, H)
-    - cache: Values needed for the backward pass.
     """
-    h, cache = None, None
-    #############################################################################
-    # TODO: Implement the forward pass for an LSTM over an entire timeseries.   #
-    # You should use the lstm_step_forward function that you just defined.      #
-    #############################################################################
-    pass
-    ##############################################################################
-    #                               END OF YOUR CODE                             #
-    ##############################################################################
+    N,T,D=x.shape
+    N,H=h0.shape
+    prev_h=h0
+    #以下的变量为反向传播时所需
+    h3=np.empty([N,T,H])
+    h4=np.empty([N,T,H])
+    I=np.empty([N,T,H])
+    F=np.empty([N,T,H])
+    O=np.empty([N,T,H])
+    G=np.empty([N,T,H])
+    NC=np.empty([N,T,H])
+    AT=np.empty([N,T,4*H])
+    h2=np.empty([N,T,H])
+    prev_c=np.zeros_like(prev_h)
+    for i in range(0, T):
+        h3[:,i,:]=prev_h
+        h4[:,i,:]=prev_c
+        #单步前向传播
+        next_h, next_c, cache_temp = lstm_step_forward(x[:,i,:], prev_h, prev_c, Wx, Wh, b)
+        prev_h=next_h
+        prev_c=next_c
+        h2[:,i,:]=prev_h
+        I[:,i,:]=cache_temp[3]
+        F[:,i,:]=cache_temp[4]
+        O[:,i,:]=cache_temp[5]
+        G[:,i,:]=cache_temp[6]
+        NC[:,i,:]=cache_temp[9]
+        AT[:,i,:]=cache_temp[10]
 
-    return h, cache
+    cache=(x,h3,h4,I,F,O,G,Wx,Wh,NC,AT)
+
+
+
+    return h2, cache
+
 
 
 def lstm_backward(dh, cache):
     """
-    Backward pass for an LSTM over an entire sequence of data.]
-
-    Inputs:
-    - dh: Upstream gradients of hidden states, of shape (N, T, H)
-    - cache: Values from the forward pass
-
-    Returns a tuple of:
-    - dx: Gradient of input data of shape (N, T, D)
-    - dh0: Gradient of initial hidden state of shape (N, H)
-    - dWx: Gradient of input-to-hidden weight matrix of shape (D, 4H)
-    - dWh: Gradient of hidden-to-hidden weight matrix of shape (H, 4H)
-    - db: Gradient of biases, of shape (4H,)
+    输入:
+    - dh: 损失函数关于每一个隐藏层的梯度, 维度 (N, T, H)
+    - cache: 前向传播时存的变量
+    输出
+    - dx: 每一层输入x的梯度, 维度(N, T, D)
+    - dh0: 初始隐藏状态的梯度, 维度(N, H)
+    - dWx: 权值矩阵Wxh的梯度, 维度(D, 4H)
+    - dWh: 权值矩阵Whh的梯度, 维度(H, 4H)
+    - db: 偏差值b的梯度，维度（4H,）
     """
-    dx, dh0, dWx, dWh, db = None, None, None, None, None
-    #############################################################################
-    # TODO: Implement the backward pass for an LSTM over an entire timeseries.  #
-    # You should use the lstm_step_backward function that you just defined.     #
-    #############################################################################
-    pass
-    ##############################################################################
-    #                               END OF YOUR CODE                             #
-    ##############################################################################
+    x=cache[0]
+    N,T,D=x.shape
+    N,T,H=dh.shape
+
+    dWx=np.zeros((D,4*H))
+    dWh=np.zeros((H,4*H))
+    db=np.zeros(4*H)
+    dout=dh
+    dx=np.empty([N,T,D])
+    hnow=np.zeros([N,H])
+    cnow=np.zeros([N,H])
+    for k in range(0, T):
+        i=T-1-k
+        hnow=hnow+dout[:,i,:]
+        cacheT=(cache[0][:,i,:],cache[1][:,i,:],cache[2][:,i,:],cache[3][:,i,:],cache[4][:,i,:],cache[5][:,i,:],cache[6][:,i,:],cache[7],cache[8],cache[9][:,i,:],cache[10][:,i,:])
+
+        dx_temp, dprev_h, dprev_c, dWx_temp, dWh_temp, db_temp = lstm_step_backward(hnow, cnow, cacheT)
+        hnow=dprev_h
+        cnow=dprev_c
+        dx[:,i,:]=dx_temp
+        dWx=dWx+dWx_temp
+        dWh=dWh+dWh_temp
+        db=db+db_temp
+
+    dh0=hnow
 
     return dx, dh0, dWx, dWh, db
+
 
 
 def temporal_affine_forward(x, w, b):
